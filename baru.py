a9bcd8ea46092d64d4ec36446808a3d496c6fd01
putrisalsabill4 <120196440+putrisalsabill4@users.noreply.github.com>
2023-01-17 00:24:12 +0700

    Add files via upload

--- baru.py
+++ baru.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import sqlite3
 from deta import Deta
 
 DETA_KEY="c0vyzncy_FtHYP14kaerkDsVKqs5eGtBBNaRVwtH7"
@@ -8,91 +7,84 @@ deta=Deta(DETA_KEY)
 
 db=deta.Base("pelanggan_db")
 
-st.header("Kelompok 6")
 st.title("DANBOX")
-option = st.selectbox(
+st.header("Kelompok 6")
+st.subheader("Aplikasi Pemesanan Makanan")
+
+with st.sidebar:
+    from PIL import Image
+     
+    image = Image.open('danbox.jpeg')
+    
+    st.image(image,caption="")
+
+    
+pilihan = st.selectbox(
     'Pilihan Pengambilan',
     ('pick up', ' delivered'))
-st.write('You selected:', option)
+st.write('You selected:', pilihan)
 
-#fungsi pesanan
-def nama(nama):
-    return db.put({"Nama":nama})
 
+#NAMA
 import streamlit as st
-nama = st.text_input('Nama Pelanggan')
+nama=st.text_input('Nama Pelanggan')
 if not nama:
     st.warning('Mohon masukkan nama.')
-    
-    
-def kelas(kelas):
-    return db.put({"Kelas":kelas})  
 
-kelas = st.text_input('Kelas')
+#KELAS    
+kelas=st.text_input('Kelas')
 if not kelas:
     st.warning('Mohon masukkan Kelas.')
 
-    
-def d(tanggal):
-    return db.put({"Tanggal":tanggal})
-
+#TANGGAL
 import datetime
 import streamlit as st
 d = st.date_input(
     " Tanggal Pemesanan",
-    datetime.date(2019, 7, 6))
+    datetime.date(2023, 1, 1))
+tgl=(d)
 
-
-def t(waktu):   
-    return db.put({"Waktu":waktu})
-import datetime
+#WAKTU
 import streamlit as st
-t = st.time_input('Waktu Pemesanan', datetime.time(8, 45))
-import streamlit as st
+t = st.time_input('Waktu Pemesanan', datetime.time(8, 30))
+if t:
+    st.write(t)
+    
 st.subheader('Menu Makanan')
 
 from PIL import Image
 import streamlit as st
-tab1, tab2, tab3, tab4, tab5= st.tabs(["Risol Mayo", "Kue Sus", "Pisang Roll","Martabak Telor","Pastel"])
 
-
-with tab1:
-    st.header("Risol Mayo")
-    image = Image.open('oip.jpg')
-    st.image(image, width=200)
-    import streamlit as st
-    number1 = st.number_input('Jumlah Risol')
-    
-
-with tab2:
-    st.header("Kue Sus")
-    image = Image.open('sss.jpg')
-    st.image(image, width=200)
-    import streamlit as st
-    number2 = st.number_input('Jumlah Kue Sus')
-    
-with tab3:
-    st.header("Pisang Roll")
-    image = Image.open('sang.jpg')
-    st.image(image, width=200)
-    import streamlit as st
-    number3 = st.number_input('Jumlah Pisang Roll')
-    
-with tab4:
-    st.header("Martabak Telor")
-    image = Image.open('mar.jpg')
-    st.image(image, width=200)
-    import streamlit as st
-    number4 = st.number_input('Jumlah Martabak Telor')    
-    
-with tab5:
-    st.header("Pastel")
-    image = Image.open('pas.jpg')
-    st.image(image, width=200)
-    import streamlit as st
-    number5 = st.number_input('Jumlah Pastel')
+col1, col2, col3=st.columns(3)
+with col1:
+    st.subheader('risol mayo')
+    st.image('don.jpg',width=150)
+    number1 = st.number_input('Jumlah Risol',0)
+with col2:
+    st.subheader('risol mayo')
+    st.image('sang.jpg',width=150)
+    number2 = st.number_input('Jumlah asfa',0)
+with col3:
+    st.subheader('risol mayo')
+    st.image('sss.jpg',width=150)
+    number3 = st.number_input('Jumlah afaetf',0)
+col4, col5, col6=st.columns(3)
+with col4:
+    st.subheader('risol mayo')
+    st.image('sus.jpg',width=150)
+    number4 = st.number_input('Jumlah Riarvwsol',0)
+with col5:
+    st.subheader('risol mayo')
+    st.image('coba.jpg',width=150)
+    number5 = st.number_input('Jumlah erc',0)
+with col6:
+    st.subheader('risol mayo')
+    st.image('bom.jpg',width=150)
+    number6 = st.number_input('Jumlah sdq',0)
     
 Total=st.button("Rincian")
+if Total:
+    st.success("pesanan berhasil")
 if Total:
     import pandas as pd
     harga1=2000*number1
@@ -100,10 +92,10 @@ if Total:
     harga3=2000*number3
     harga4=2000*number4
     harga5=2000*number5
-    data = [['Risol',number1,harga1],['Keu Sus',number2,harga2],['Pisang Roll',number3,harga3],['Martabak Telor',number4,harga4],['Pastel',number5,harga5]]
+    harga6=2000*number6
+    data = [['Risol',number1,harga1],['Keu Sus',number2,harga2],['Pisang Roll',number3,harga3],['Martabak Telor',number4,harga4],['Pastel',number5,harga5],['kue sus buah',number6,harga6]]
     df=pd.DataFrame(data,columns=['Makanan','jumlah','harga (Rp.)'])
     df
-
 
 harga1=2000
 harga2=3000
@@ -117,47 +109,52 @@ if hitung1>20000:
     hitung1=hitung1-0.1*hitung1
     st.write("Diskon 10% total harga makanan Rp.",hitung1)
 
+def pesanan(pilihan,namapel,kelas,tanggal,waktu,menu):
+    return db.put({"Pilihan":pilihan,"Nama":namapel,"Kelas":kelas,"Tanggal":tanggal,"Waktu":waktu,"Menu":menu})
+
+pesanan(pilihan,nama,kelas,"Januari","20.30","menu")    
+
 import streamlit as st
 st.subheader('Perlengkapan Laboratorium')
 from PIL import Image
 import streamlit as st
-tab1, tab2, tab3, tab4, tab5= st.tabs(["Sarung Tangan", " Serbet", "Tabung Reaksi","Gelas Piala","Tabung Ulir"])
-
-with tab1:
+col7, col8, col9=st.columns(3)
+with col7:
     st.header("Sarung Tangan")
     image = Image.open('sarung.jpg')
-    st.image(image, width=200)
+    st.image(image, width=150)
     import streamlit as st
-    numberA = st.number_input('Jumlah sarung tangan')
+    numberA = st.number_input('Jumlah sarung tangan',0)
     
 
-with tab2:
+with col8:
     st.header("Serbet")
     image = Image.open('serbet.jpg')
-    st.image(image, width=200)
+    st.image(image, width=150)
     import streamlit as st
-    numberB = st.number_input('Jumlah serbet')
+    numberB = st.number_input('Jumlah serbet',0)
     
-with tab3:
+with col9:
     st.header("Tabung Reaksi")
     image = Image.open('tr.jpg')
-    st.image(image, width=200)
+    st.image(image, width=150)
     import streamlit as st
-    numberC = st.number_input('Jumlah tabung reaksi')
+    numberC = st.number_input('Jumlah tabung reaksi',0)
     
-with tab4:
+col10, col11, col12=st.columns(3)
+with col10:
     st.header("Gelas Piala")
     image = Image.open('piala.jpg')
-    st.image(image, width=200)
+    st.image(image, width=150)
     import streamlit as st
-    numberD = st.number_input('Jumlah Gelas Piala')    
+    numberD = st.number_input('Jumlah Gelas Piala',0)    
     
-with tab5:
+with col11:
     st.header("Tabung Ulir")
     image = Image.open('lir.jpg')
-    st.image(image, width=200)
+    st.image(image, width=150)
     import streamlit as st
-    numberE = st.number_input('Jumlah tabung ulir')
+    numberE = st.number_input('Jumlah tabung ulir',0)
     
 lab = st.button("rincian")
 if lab:
--- danusan.py
+++ danusan.py
@@ -1,6 +1,15 @@
 import streamlit as st
+st.sidebar.header("Kolom Pencarian")
+text=st.sidebar.text_area("Cari produk di sini")
+button1=st.sidebar.button("Masukkan")
+if button1:
+    st.write(text) 
+    clean=clean_text(text)
+    st.write(clean)
 st.header("Kelompok 6")
 st.title("DANBOX")
+ 
+    
 option = st.selectbox(
     'Pilihan Pengambilan',
     ('pick up', ' delivered'))
@@ -10,10 +19,10 @@ st.write('You selected:', option)
 import streamlit as st
 name = st.text_input('Nama Pelanggan')
 if not name:
-    st.warning('Please input a nama.')
+    st.warning('Please input a name.')
 name = st.text_input('Kelas')
 if not name:
-    st.warning('Please input a Kelas.')
+    st.warning('Please input a class.')
 import datetime
 import streamlit as st
 d = st.date_input(
@@ -27,46 +36,36 @@ import streamlit as st
 st.subheader('Menu Makanan')
 from PIL import Image
 import streamlit as st
-tab1, tab2, tab3, tab4, tab5= st.tabs(["Risol Mayo", "Kue Sus", "Pisang Roll","Martabak Telor","Pastel"])
 
-with tab1:
-    st.header("Risol Mayo")
-    image = Image.open('oip.jpg')
-    st.image(image, width=200)
-    import streamlit as st
-    number1 = st.number_input('Jumlah Risol')
-    
-
-with tab2:
-    st.header("Kue Sus")
-    image = Image.open('sss.jpg')
-    st.image(image, width=200)
-    import streamlit as st
-    number2 = st.number_input('Jumlah Kue Sus')
-    
-with tab3:
-    st.header("Pisang Roll")
-    image = Image.open('sang.jpg')
-    st.image(image, width=200)
-    import streamlit as st
-    number3 = st.number_input('Jumlah Pisang Roll')
-    
-with tab4:
-    st.header("Martabak Telor")
-    image = Image.open('mar.jpg')
-    st.image(image, width=200)
-    import streamlit as st
-    number4 = st.number_input('Jumlah Martabak Telor')    
-    
-with tab5:
-    st.header("Pastel")
-    image = Image.open('pas.jpg')
-    st.image(image, width=200)
-    import streamlit as st
-    number5 = st.number_input('Jumlah Pastel')
-    
-Total=st.button("Rincian")
-if Total:
+col1, col2, col3=st.columns(3)
+with col1:
+    st.subheader('risol mayo')
+    st.image('don.jpg',width=150)
+    number1 = st.number_input('Jumlah Risol',0)
+with col2:
+    st.subheader('risol mayo')
+    st.image('sang.jpg',width=150)
+    number2 = st.number_input('Jumlah asfa',0)
+with col3:
+    st.subheader('risol mayo')
+    st.image('sss.jpg',width=150)
+    number3 = st.number_input('Jumlah afaetf',0)
+col4, col5, col6=st.columns(3)
+with col4:
+    st.subheader('risol mayo')
+    st.image('sus.jpg',width=150)
+    number4 = st.number_input('Jumlah Riarvwsol',0)
+with col5:
+    st.subheader('risol mayo')
+    st.image('coba.jpg',width=150)
+    number5 = st.number_input('Jumlah erc',0)
+with col6:
+    st.subheader('risol mayo')
+    st.image('bom.jpg',width=150)
+    number5 = st.number_input('Jumlah sdq',0)
+Total=st.button ("Rincian")
+lah=number1+number2+number3+number4+number5
+while lah>0:
     import pandas as pd
     harga1=2000*number1
     harga2=3000*number2
@@ -76,7 +75,10 @@ if Total:
     data = [['Risol',number1,harga1],['Keu Sus',number2,harga2],['Pisang Roll',number3,harga3],['Martabak Telor',number4,harga4],['Pastel',number5,harga5]]
     df=pd.DataFrame(data,columns=['Makanan','jumlah','harga (Rp.)'])
     df
-
+    break
+else:
+    lah<0.01
+    st.warning('Tidak ada pesanan')
 
 harga1=2000
 harga2=3000
@@ -94,46 +96,48 @@ import streamlit as st
 st.subheader('Perlengkapan Laboratorium')
 from PIL import Image
 import streamlit as st
-tab1, tab2, tab3, tab4, tab5= st.tabs(["Sarung Tangan", " Serbet", "Tabung Reaksi","Gelas Piala","Tabung Ulir"])
 
-with tab1:
+col7, col8, col9=st.columns(3)
+with col7:
     st.header("Sarung Tangan")
     image = Image.open('sarung.jpg')
-    st.image(image, width=200)
+    st.image(image, width=150)
     import streamlit as st
-    numberA = st.number_input('Jumlah sarung tangan')
+    numberA = st.number_input('Jumlah sarung tangan',0)
     
 
-with tab2:
+with col8:
     st.header("Serbet")
     image = Image.open('serbet.jpg')
-    st.image(image, width=200)
+    st.image(image, width=150)
     import streamlit as st
-    numberB = st.number_input('Jumlah serbet')
+    numberB = st.number_input('Jumlah serbet',0)
     
-with tab3:
+with col9:
     st.header("Tabung Reaksi")
     image = Image.open('tr.jpg')
-    st.image(image, width=200)
+    st.image(image, width=150)
     import streamlit as st
-    numberC = st.number_input('Jumlah tabung reaksi')
+    numberC = st.number_input('Jumlah tabung reaksi',0)
     
-with tab4:
+col10, col11, col12=st.columns(3)
+with col10:
     st.header("Gelas Piala")
     image = Image.open('piala.jpg')
-    st.image(image, width=200)
+    st.image(image, width=150)
     import streamlit as st
-    numberD = st.number_input('Jumlah Gelas Piala')    
+    numberD = st.number_input('Jumlah Gelas Piala',0)    
     
-with tab5:
+with col11:
     st.header("Tabung Ulir")
     image = Image.open('lir.jpg')
-    st.image(image, width=200)
+    st.image(image, width=150)
     import streamlit as st
-    numberE = st.number_input('Jumlah tabung ulir')
+    numberE = st.number_input('Jumlah tabung ulir',0)
     
 lab = st.button("rincian")
-if lab:
+jml=numberA+numberB+numberC+numberD+numberE
+while jml>0:
     import pandas as pd
     hargaA=2000*numberA
     hargaB=5000*numberB
@@ -143,9 +147,14 @@ if lab:
     data = [['Sarung Tangan',numberA,hargaA],['Serbet',numberB,hargaB],['Tabung Reaksi',numberC,hargaC],['Gelas Piala',numberD,hargaD],['Tabung Ulir',numberE,hargaE]]
     df=pd.DataFrame(data,columns=['Perlengkapan','jumlah','harga (Rp.)'])
     df
-
-
-hargaA=2000
+    break
+    import streamlit as st
+else:
+    jml<0.1
+    st.warning('Tidak ada pesanan')
+    
+   
+    hargaA=2
 hargaB=5000
 hargaC=10000
 hargaD=20000
@@ -156,6 +165,8 @@ if hitung<30000:
 if hitung>30000:
     hitung=hitung-0.1*hitung
     st.write("Diskon 10% total harga perlengkapan Rp.",hitung)
+    
+st.write('-----------------------------------------------------------------------------------------------------------------------------')
 
 seluruh=hitung1+hitung
 st.write('Total Harga keseluruhan Rp.',seluruh)
@@ -165,5 +176,6 @@ agree = st.checkbox('Saya setuju')
 if agree:
     st.write('Terima kasih atas pesanannya!')
 
+jumlah=int(input('banyak makanana'))
 
 
